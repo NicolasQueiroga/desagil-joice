36159acbb932d3af2edceadb734b0d5bb226520e
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-04-04 13:50:08 -0300

    criando a pasta de test para as classes CalculaCFC e CAlculaCCC

---
+++ test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
@@ -0,0 +1,16 @@
+package br.edu.insper.desagil.joice.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraCCCTest 
+{
+
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/br/edu/insper/desagil/joice/model/CalculadoraCFCTest.java
@@ -0,0 +1,16 @@
+package br.edu.insper.desagil.joice.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraCFCTest 
+{
+
+	@Test
+	void test() 
+	{
+		fail("Not yet implemented");
+	}
+
+}
