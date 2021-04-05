0b87f64ad7710ef0cfdb429f95ee24477f9a54f1
fran-janela <francisco.pinheiro.janela@gmail.com>
2021-04-05 13:12:14 -0300

    correções no Teste, calclaCFC feito e reorganização de parâmetros

--- src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
+++ src/br/edu/insper/desagil/joice/model/CalculadoraCFC.java
@@ -3,17 +3,20 @@ package br.edu.insper.desagil.joice.model;
 public class CalculadoraCFC extends Calculadora 
 {
 
-	public CalculadoraCFC(String nome) 
+	public CalculadoraCFC() 
 	{
-		super(nome);
-		// TODO Auto-generated constructor stub
+		super("Cúbica de Face Centrada");
 	}
 
 	@Override
 	public double calcula(double peso, double raio) 
 	{
-		// TODO Auto-generated method stub
-		return 0;
+		raio = raio*Math.pow(10, -7);
+	    double V = (16*Math.pow(raio, 3))*(Math.sqrt(2));
+	    double Na = 6.02*Math.pow(10, 23);
+	    double num = 4*peso;
+	    double den = V*Na;
+		return num/den;
 	}
 
 }
