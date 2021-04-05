f918033a720cf2addaca2499c535a4c59e465885
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-04-05 02:35:43 -0300

    fazendo casos de teste de estruturas CCC

--- test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
+++ test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
@@ -1,16 +1,38 @@
 package br.edu.insper.desagil.joice.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class CalculadoraCCCTest 
 {
-
+	
 	@Test
-	void test() 
+	public void densidadeNiobio() 
 	{
-		fail("Not yet implemented");
+		Calculadora CCC = new CalculadoraCCC("Nióbio");
+		double out = CCC.calcula(92.91, 0.143);
+		double expected = 8.570176381874958;
+		assertEquals(expected, out, 0.00001);
+		
+	}
+	
+	@Test
+	public void densidadeTungstenio() 
+	{
+		Calculadora CCC = new CalculadoraCCC("Tungstênio");
+		double out = CCC.calcula(183.84, 0.137);
+		double expected = 19.28473708545127;
+		assertEquals(expected, out, 0.00001);
+	}
+	
+	@Test
+	public void densidadeLitio() 
+	{
+		Calculadora CCC = new CalculadoraCCC("Lítio");
+		double out = CCC.calcula(6.94, 0.152);
+		double expected = 0.5330453008680993;
+		assertEquals(expected, out, 0.00001);
 	}
 
 }
