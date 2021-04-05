b5ce7caf2f9b3fa1b7dc92d5f9ad59386d9a1803
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-04-05 02:39:47 -0300

    ajeitando tolerancia do asserEquals()

--- test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
+++ test/br/edu/insper/desagil/joice/model/CalculadoraCCCTest.java
@@ -12,8 +12,8 @@ class CalculadoraCCCTest
 	{
 		Calculadora CCC = new CalculadoraCCC("Nióbio");
 		double out = CCC.calcula(92.91, 0.143);
-		double expected = 8.570176381874958;
-		assertEquals(expected, out, 0.00001);
+		double expected = 8.57;
+		assertEquals(expected, out, 0.05);
 		
 	}
 	
@@ -22,8 +22,8 @@ class CalculadoraCCCTest
 	{
 		Calculadora CCC = new CalculadoraCCC("Tungstênio");
 		double out = CCC.calcula(183.84, 0.137);
-		double expected = 19.28473708545127;
-		assertEquals(expected, out, 0.00001);
+		double expected = 19.28;
+		assertEquals(expected, out, 0.05);
 	}
 	
 	@Test
@@ -31,8 +31,8 @@ class CalculadoraCCCTest
 	{
 		Calculadora CCC = new CalculadoraCCC("Lítio");
 		double out = CCC.calcula(6.94, 0.152);
-		double expected = 0.5330453008680993;
-		assertEquals(expected, out, 0.00001);
+		double expected = 0.53;
+		assertEquals(expected, out, 0.05);
 	}
 
 }
